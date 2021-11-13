fd1774c53a514430b1f528b6236c684c96c56683
ygleeee <80654538+ygleeee@users.noreply.github.com>
2021-11-14 08:33:48 +0900

    p35 의미있는 맥락을 추가하라 (목록 2-1)

---
+++ src/main/java/ch2/meaningfulContext.java
@@ -0,0 +1,45 @@
+package ch2;
+
+public class meaningfulContext {
+
+   public static void main(String[] args) {
+
+      pringGuessStatistics('a', 0);
+
+   }
+
+   private static void pringGuessStatistics(char candidate, int count){
+
+      String number;
+      String verb;
+      String pluralModifier;
+
+
+      if(count == 0){
+
+         number = "no";
+         verb = "are";
+         pluralModifier = "s";
+
+      } else if(count == 1){
+
+         number = "1";
+         verb = "is";
+         pluralModifier = "";
+
+      } else {
+
+         number = Integer.toString(count);
+         verb = "are";
+         pluralModifier = "s";
+      }
+
+      String guessMessage = String.format("There %s %s %s%s", verb, number, candidate, pluralModifier);
+      System.out.println(guessMessage);
+   }
+
+
+
+
+
+}
